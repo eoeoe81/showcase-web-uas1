a54b9969e9db157543d1ddd4dcddaa7251a54a6b
jessicaleo <kentofukuyama58@gmail.com>
2025-12-04 10:50:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (navbarCollapse.classList.contains("show")) {
+        new bootstrap.Collapse(navbarCollapse).hide();
+      }
+    });
+  });
+
+  console.log("Website Portofolio Siap!");
+});
